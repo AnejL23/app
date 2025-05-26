8e2ae74a0f4b6e5cc8a615682260858748f0d4e1
Anej Lampret <74080259+AnejL23@users.noreply.github.com>
2025-05-26 15:30:54 +0200

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@
 export default function Header () {
     return (
         <header className="max-w-md">
-                <h1 className="text-white text-2xl font-bold">Fittnes: EndGame</h1>
+                <h1 className="text-white text-2xl font-bold">Fitness: EndGame</h1>
                 <p className="text-gray-500">Guess the word under 8 attempts to keep 
                     the fitness community safe from cardio</p>
         </header>
